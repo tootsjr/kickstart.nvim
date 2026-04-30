7ce5df262252092408a4f662812be53fc47f05ea
Bjorn Silvester <bjorn.silvester@gmail.com>
2026-04-30 18:49:10 +0200

    fix: keybinding conflict

--- lua/custom/plugins/spectre.lua
+++ lua/custom/plugins/spectre.lua
@@ -5,9 +5,9 @@ return {
     'nvim-pack/nvim-spectre',
     dependencies = { 'nvim-lua/plenary.nvim' },
     keys = {
-      { '<leader>R', '<cmd>lua require("spectre").toggle()<CR>', desc = '[R]eplace in project (Spectre)' },
-      { '<leader>Rw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', mode = { 'n', 'v' }, desc = '[R]eplace current [w]ord' },
-      { '<leader>Rf', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', desc = '[R]eplace in current [f]ile' },
+      { '<leader>R',  function() require('spectre').toggle() end,                              desc = '[R]eplace in project (Spectre)' },
+      { '<leader>Rw', function() require('spectre').open_visual { select_word = true } end,    mode = { 'n', 'v' }, desc = '[R]eplace current [w]ord' },
+      { '<leader>Rf', function() require('spectre').open_file_search { select_word = true } end, desc = '[R]eplace in current [f]ile' },
     },
     opts = { open_cmd = 'noswapfile vnew' },
   },
